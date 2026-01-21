74583ea3408e67e4bb1c1add59b52dc9f08de40d
Dmitry Voloskov <voloskovdmitriy@gmail.com>
2026-01-21 11:33:40 +0300

    exports

--- resdp/__init__.py
+++ resdp/__init__.py
@@ -3,3 +3,5 @@
 from ._load_utils import load as load
 from ._dump_utils import dump as dump
 from ._data_directory import DATA_DIRECTORY as DATA_DIRECTORY
+from ._data_directory import ValueType as ValueType
+from ._data_directory import DataType as DataType
