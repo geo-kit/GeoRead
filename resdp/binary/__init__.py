c398e3c773647221b77ed6831b02ca7a2de3f86b
Dmitry Voloskov <voloskovdmitriy@gmail.com>
2026-02-01 17:04:22 +0300

    tests

--- resdp/binary/__init__.py
+++ resdp/binary/__init__.py
@@ -3,3 +3,4 @@
 from ._binary_data import load as load
 from ._binary_data import BinaryData as BinaryData
 from ._binary_data import FileType as FileType
+from ._binary_data import BinaryFileData as BinaryFileData
